15b7fd8dc5b07c5b40acf8a0cd3a30e51233bf9f
Zanexywex <thanarat.wex@gmail.com>
2025-09-18 01:11:45 +0700

    Update ESANS.lua

--- ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/ESANS.lua
+++ ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/ESANS.lua
@@ -1,7 +1,10 @@
 local gd = game.GameId
+local github = "https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/"
 
-if gd == 8032151497 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/One%20Piece%20Mythic.lua"))()
-elseif gd == 7750955984 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/HuntyZombie.lua"))()
-end
+loadstring(game:HttpGet(github..tostring(gd))()
+
+-- if gd == 8032151497 then
+--   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/One%20Piece%20Mythic.lua"))()
+-- elseif gd == 7750955984 then
+--   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/HuntyZombie.lua"))()
+-- end
