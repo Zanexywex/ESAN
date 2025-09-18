b68cee85201303db60f665cf171818c2e1095666
Zanexywex <thanarat.wex@gmail.com>
2025-09-18 17:19:07 +0700

    Update ESANS.lua

--- ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/ESANS.lua
+++ ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/ESANS.lua
@@ -1,7 +1,9 @@
 local gd = game.GameId
-local github = "https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/"
+local github = "https://raw.githubusercontent.com/Zanexywex/ESAN/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/"
+local url = github .. tostring(gd)
+local scriptString = game:HttpGet(url)
+loadstring(scriptString)()
 
-loadstring(game:HttpGet(github..tostring(gd))()
 
 -- if gd == 8032151497 then
 --   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/ESAN/refs/heads/main/ESAN/ESAN/ESAN/EASN/EAS/EASN/ESAN/ESAN/One%20Piece%20Mythic.lua"))()
